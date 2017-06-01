56a8f258b7d0f7ac8bdfa7d8ca100019270eab01
Zet God <usr1@Usr1s-MacBook-Pro.local>
2017-06-01 12:45:46 +0200

    dodanie pliku JS z triangleArea i pliku .gitignore

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var a = 10;
+var h = 5.2;
+var triangeArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangeArea);
+
+//niżej funkcja
+
+function triangleArea(a,h) {
+	var area = a*h/2;
+	console.log(area);
+}
